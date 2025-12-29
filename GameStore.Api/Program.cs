12e632c8942c9b74b17cb5362faf2209f8faddc0
grahamkatana <graykatanakenny@gmail.com>
2025-12-29 17:56:11 +0200

    Refactor routes

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,65 +1,9 @@
 using GameStore.Api.Dtos;
-const string EndpointName = "GetGameById";
+using GameStore.Api.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.MapGamesEndpoints();
 
-List<GameDto> games = [
-    new (1, "The Legend of Zelda: Breath of the Wild", "Action-adventure", 59.99m, new DateOnly(2017, 3, 3)),
-    new (2, "God of War", "Action-adventure", 49.99m, new DateOnly(2018, 4, 20)),
-    new (3, "Red Dead Redemption 2", "Action-adventure", 69.99m, new DateOnly(2018, 10, 26)),
-    new (4, "Minecraft", "Sandbox", 26.95m, new DateOnly(2011, 11, 18)),
-    new (5, "The Witcher 3: Wild Hunt", "Action RPG", 39.99m, new DateOnly(2015, 5, 19))
-];
-
-
-// GET /games
-app.MapGet("/games", () => games);
-
-
-
-// GET /games/{id}
-app.MapGet("/games/{id}", (int id) =>
-{
-    var game = games.FirstOrDefault(g => g.Id == id);
-    return game is not null ? Results.Ok(game) : Results.NotFound();
-}).WithName(EndpointName);
-// POST /games
-app.MapPost("/games", (GameCreateDto newGame) =>
-{
-    var nextId = games.Max(g => g.Id) + 1;
-    var gameDto = new GameDto(
-        nextId,
-        newGame.Title,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-    );
-    games.Add(gameDto);
-    return Results.CreatedAtRoute(EndpointName, new { id = gameDto.Id }, gameDto);
-});
-// PATCH /games/{id}
-app.MapPatch("/games/{id}", (int id, GameUpdateDto updatedGame) =>
-{
-
-    var index = games.FindIndex(g => g.Id == id);
-    if (index == -1) return Results.NotFound();
-    games[index] = new GameDto(
-        id,
-        updatedGame.Title,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-    return Results.Ok();
-
-});
-// DELETE /games/{id}
-app.MapDelete("/games/{id}", (int id) =>
-{
-    var index = games.FindIndex(g => g.Id == id);
-    if (index == -1) return Results.NotFound();
-    games.RemoveAt(index);
-    return Results.NoContent();
-});
 
 app.Run();
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637802e728d22e8165e8d674964e3e439063e0af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f1f3d4ddadfd1296dc41403d9ca3d4b8a165e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
