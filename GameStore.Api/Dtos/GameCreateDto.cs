23d01686c5151d9518f2c41973f4f6327db735f1
grahamkatana <graykatanakenny@gmail.com>
2025-12-29 18:58:02 +0200

    Validate requests

--- GameStore.Api/Dtos/GameCreateDto.cs
+++ GameStore.Api/Dtos/GameCreateDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
 public record GameCreateDto(
-    string Title,
-    string Genre,
-    decimal Price,
-    DateOnly ReleaseDate
+    [Required][StringLength(50)][MinLength(3)] string Title,
+    [Required][StringLength(20)] string Genre,
+    [Required][Range(0, 1000)] decimal Price,
+    [Required] DateOnly ReleaseDate
 );
 
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -2,6 +2,7 @@ using GameStore.Api.Dtos;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddValidation();
 var app = builder.Build();
 app.MapGamesEndpoints();
 
